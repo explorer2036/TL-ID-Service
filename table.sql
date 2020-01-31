092ba35cae5a3f5b9557e61044fb18abee215007
alonlong <alonlong@163.com>
2020-01-31 19:53:57 +0800

    update the table sql

--- table.sql
+++ table.sql
@@ -1,7 +1,3 @@
-CREATE SEQUENCE tl_user_sequence
-  start 100000000
-  increment 1;
+CREATE SEQUENCE user_id_sequence start 100000000 increment 1;
 
-CREATE TABLE tl_user (
-  id SERIAL PRIMARY KEY
-);
+CREATE TABLE tl_user (id SERIAL PRIMARY KEY);
